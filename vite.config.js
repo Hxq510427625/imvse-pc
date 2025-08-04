4532f6fe53407a389bf62154c4972b7fe1884577
huangxiaoqian <510427625@qq.com>
2025-08-05 01:48:55 +0800

    open /home.html2

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/imvse-pc/',
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000,          
+  server: {       
     open: '/home.html'       
   }
 })
